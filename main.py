ee0f70e0c8038ad79e4158ba589d8dcd0d2ed6f3
iamZekky <98856142+iamZekky@users.noreply.github.com>
2024-01-09 03:13:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,69 @@
-import os
-import sys
+import config
 import json
 import time
+import websocket
 import requests
-import websockets  # Add this line
-from keep_alive import keep_alive
 
-status = "dnd"  # online/dnd/idle
-
-custom_status = "Discord.gg/SpicyCode"  # If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
-
-usertoken = os.getenv("TOKEN")
-if not usertoken:
-    print("[ERROR] Please add a token inside Secrets.")
-    sys.exit()
-
-headers = {"Authorization": usertoken, "Content-Type": "application/json"}
-
-validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
-if validate.status_code != 200:
-    print("[ERROR] Your token might be invalid. Please check it again.")
-    sys.exit()
-
+status = "dnd"
+token = config.token
+custom_status = "Discord.gg/SpicyCode"
+headers = {"Authorization": token, "Content-Type": "application/json"}
 userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
 username = userinfo["username"]
 discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
-async def onliner(token, status):
-    uri = "wss://gateway.discord.gg/?v=9&encoding=json"
-    async with websockets.connect(uri) as ws:
-        start = json.loads(await ws.recv())
-        heartbeat = start["d"]["heartbeat_interval"]
-        
-        auth = {
-            "op": 2,
-            "d": {
-                "token": token,
-                "properties": {
-                    "$os": "Windows 10",
-                    "$browser": "Google Chrome",
-                    "$device": "Windows",
-                },
-                "presence": {"status": status, "afk": False},
-            },
-            "s": None,
-            "t": None,
-        }
-        await ws.send(json.dumps(auth))
-        
-        cstatus = {
-            "op": 3,
-            "d": {
-                "since": 0,
-                "activities": [
-                    {
-                        "type": 4,
-                        "state": custom_status,
-                        "name": "Custom Status",
-                        "id": "custom",
-                    }
-                ],
-                "status": status,
-                "afk": False,
-            },
-        }
-        await ws.send(json.dumps(cstatus))
-        
-        online = {"op": 1, "d": "None"}
-        await asyncio.sleep(heartbeat / 1000)
-        await ws.send(json.dumps(online))
+cstatus = {
+        "op": 3,
+        "d": {
+            "since": 0,
+            "activities": [
+                {
+                    "type": 4,
+                    "state": custom_status,
+                    "name": "Custom Status",
+                    "id": "custom",
+                    #Uncomment the below lines if you want an emoji in the status
+                    "emoji": {
+                        "name": "dev2",
+                        "id": "963192571830599710",
+                        "animated": False,
+                    },
+                }
+            ],
+            "status": status,
+            "afk": False,
+        },
+    }
 
-async def run_onliner():
+def keep_online(token, status):
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    start = json.loads(ws.recv())
+    heartbeat = start['d']['heartbeat_interval']
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows",
+            },
+            "presence": {"status": status, "afk": False},
+        },
+        "s": None,
+        "t": None,
+    }
+    ws.send(json.dumps(auth))
+    ws.send(json.dumps(cstatus))  # Add this line to set custom status
+    time.sleep(heartbeat / 1000)
+    ws.send(json.dumps({"op": 1, "d": None}))
+
+def run_keep_online():
     print(f"Logged in as {username}#{discriminator} ({userid}).")
     while True:
-        await onliner(usertoken, status)
-        await asyncio.sleep(30)
+        keep_online(token, status)
+        time.sleep(30)
 
-import asyncio
-asyncio.run(run_onliner())
-asyncio.run(keep_alive())
+run_keep_online()
